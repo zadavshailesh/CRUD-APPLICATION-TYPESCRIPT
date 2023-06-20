491a5923a58d839cd3f055aeaae29367f44ac503
Shailesh <zadavshailesh@gmail.com>
2023-06-20 08:10:26 +0545

    adding authorization

--- src/routes/employeeRoute.ts
+++ src/routes/employeeRoute.ts
@@ -13,9 +13,9 @@ import {auth} from "../middleware/auth"
 const employeeRouter: Router = express.Router();
 
 employeeRouter.post('/login/employee',auth,insert); 
-employeeRouter.get('/employees', read);
-employeeRouter.get('/employees/:id', readById);
-employeeRouter.put('/employee/:id', update);
-employeeRouter.delete('/employee/:id', deleteEmployee);
+employeeRouter.get('/employees', auth,read);
+employeeRouter.get('/employees/:id', auth, readById);
+employeeRouter.put('/employee/:id', auth, update);
+employeeRouter.delete('/employee/:id', auth, deleteEmployee);
 
 export default employeeRouter;
